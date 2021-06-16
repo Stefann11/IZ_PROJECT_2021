42a5c04da836d6c662b6e3044b7c39c875194be0
Stefann11 <santrac.stefan@gmail.com>
2021-06-16 15:35:18 +0200

    feat: initialize project

---
+++ InzenjeringProject/InzenjeringProject/src/main/java/com/example/InzenjeringProject/InzenjeringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.InzenjeringProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InzenjeringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InzenjeringProjectApplication.class, args);
+	}
+
+}
