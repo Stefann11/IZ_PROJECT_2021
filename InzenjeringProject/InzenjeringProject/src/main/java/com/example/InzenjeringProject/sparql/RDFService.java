c58f8e1aa4c7610c2b010dd044ef2b511aaa9ad6
Nemanja Dimitrijevic <necapro@gmail.com>
2021-06-18 14:29:49 +0200

    fix: Fix RDF delete

--- InzenjeringProject/InzenjeringProject/src/main/java/com/example/InzenjeringProject/sparql/RDFService.java
+++ InzenjeringProject/InzenjeringProject/src/main/java/com/example/InzenjeringProject/sparql/RDFService.java
@@ -8,7 +8,6 @@ import org.apache.jena.update.UpdateExecutionFactory;
 import org.apache.jena.update.UpdateFactory;
 import org.apache.jena.update.UpdateProcessor;
 import org.apache.jena.update.UpdateRequest;
-import org.apache.jena.vocabulary.RDF;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -34,8 +33,8 @@ public class RDFService {
                 "	   attacks:prerequisites \"" + attack.getPrerequisites() + "\"^^xsd:string;" +
                 "	   attacks:typical_severity \"" + attack.getTypical_severity() + "\"^^xsd:string;" +
                 "}";
-        System.out.println(insertString);
         UpdateRequest updateRequest = UpdateFactory.create(insertString);
+        System.setProperty("http.maxConnections", "10000");
         UpdateProcessor updateProcessor = UpdateExecutionFactory.createRemote(updateRequest, UPDATE_URL);
         updateProcessor.execute();
         return  attack;
@@ -59,6 +58,7 @@ public class RDFService {
                 "  \t\tattacks:typical_severity ?typical_severity.}";
         Query query = QueryFactory.create(queryString) ;
         try {
+            System.setProperty("http.maxConnections", "10000");
             QueryExecution qexec = QueryExecutionFactory.sparqlService(QUERY_URL, query);
 
             ResultSet results = qexec.execSelect() ;
@@ -86,24 +86,15 @@ public class RDFService {
     }
 
     public RDFAttackDTO delete(RDFAttackDTO attack) {
-        String insertString = ""
-                + "PREFIX attacks: <http://www.ftn.uns.ac.rs/attacks#> "
-                + "PREFIX xsd:   <http://w3.org/2001/XMLSchema#> "
-                + "INSERT DATA {"
-                + "    attacks:" + LocalDateTime.now() + " a attacks:Attack;" +
-                "	   attacks:availability \"" + attack.getAvailability() + "\"^^xsd:string;" +
-                "	   attacks:confidentiality \"" + attack.getConfidentiality() + "\"^^xsd:string;" +
-                "	   attacks:confidentiality_access_control_authorization \"" + attack.getConfidentiality_access_control_authorization() + "\"^^xsd:string;" +
-                "	   attacks:likelihood_of_attack \"" + attack.getLikelihood_of_attack() + "\"^^xsd:string;" +
-                "	   attacks:mitigations \"asd\"^^xsd:string;" +
-                "	   attacks:name \"asd\"^^xsd:string;" +
-                "	   attacks:prerequisites \"" + attack.getPrerequisites() + "\"^^xsd:string;" +
-                "	   attacks:typical_severity \"" + attack.getTypical_severity() + "\"^^xsd:string;" +
-                "}";
-        System.out.println(insertString);
-        UpdateRequest updateRequest = UpdateFactory.create(insertString);
-        UpdateProcessor updateProcessor = UpdateExecutionFactory.createRemote(updateRequest, UPDATE_URL);
-        updateProcessor.execute();
+        String deleteString =  "PREFIX attacks: <http://www.ftn.uns.ac.rs/attacks#> "
+        		+ "DELETE "
+        		+ "WHERE {"
+        		+ "    attacks:" + attack.getAttack() + " ?x ?y ."
+        		+ "}";
+		UpdateRequest updateRequest2 = UpdateFactory.create(deleteString);
+        System.setProperty("http.maxConnections", "10000");
+        UpdateProcessor updateProcessor2 = UpdateExecutionFactory.createRemote(updateRequest2, UPDATE_URL);
+        updateProcessor2.execute();
         return  attack;
     }
 
@@ -118,16 +109,16 @@ public class RDFService {
                 "	   attacks:confidentiality \"" + attack.getConfidentiality() + "\"^^xsd:string;" +
                 "	   attacks:confidentiality_access_control_authorization \"" + attack.getConfidentiality_access_control_authorization() + "\"^^xsd:string;" +
                 "	   attacks:likelihood_of_attack \"" + attack.getLikelihood_of_attack() + "\"^^xsd:string;" +
-                "	   attacks:mitigations \"asd\"^^xsd:string;" +
-                "	   attacks:name \"asd\"^^xsd:string;" +
+                "	   attacks:mitigations \"" + attack.getMitigations() + "\"^^xsd:string;" +
+                "	   attacks:name \"" + attack.getName() + "\"^^xsd:string;" +
                 "	   attacks:prerequisites \"" + attack.getPrerequisites() + "\"^^xsd:string;" +
                 "	   attacks:typical_severity \"" + attack.getTypical_severity() + "\"^^xsd:string;" +
                 "}"
                 + "WHERE {"
                 + "    attacks:" + attack.getAttack() + " ?x ?y ."
                 + "}";
-        System.out.println(insertString);
         UpdateRequest updateRequest = UpdateFactory.create(insertString);
+        System.setProperty("http.maxConnections", "10000");
         UpdateProcessor updateProcessor = UpdateExecutionFactory.createRemote(updateRequest, UPDATE_URL);
         updateProcessor.execute();
         return  attack;
